ba19abba4b574984de1c518e119ebb98d1588138
Abby Cocker <evelynccr1@gmail.com>
2024-02-05 13:25:45 -0500

    Tier One complete :-)

--- tier-one/src/main/java/lu/playground/service/AccountService.java
+++ tier-one/src/main/java/lu/playground/service/AccountService.java
@@ -1,5 +1,7 @@
 package lu.playground.service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.validation.constraints.NotBlank;
@@ -7,6 +9,7 @@ import javax.validation.constraints.NotNull;
 
 import lombok.extern.slf4j.Slf4j;
 import lu.playground.entity.Account;
+import lu.playground.exception.DatabaseException;
 import lu.playground.service.contracts.AccountServiceContract;
 
 /**
@@ -16,31 +19,73 @@ import lu.playground.service.contracts.AccountServiceContract;
 @Slf4j
 public class AccountService implements AccountServiceContract {
     
+    private Map<String, Account> accountDatabase = new HashMap<>();
+    
     public AccountService(){
         log.info("Initializing the Account Service...");
     }
 
     @Override
     public String createAccount(@NotBlank String firstName, @NotBlank String lastName) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createAccount'");
-    }
+        try{
+            String accountID = firstName +lastName;
 
+            if(accountDatabase.containsKey(accountID)){
+                log.info("Account already exists. Account ID: {}", accountID);
+                return accountID;
+            }
+
+            Account newAccount = new Account(firstName, lastName);
+            accountDatabase.put(accountID, newAccount);
+
+            log.info("Account created. Account ID: {}", accountID);
+            return accountID;
+        } catch (Exception e){
+            log.error("Could not create account", e);
+            throw new DatabaseException("Could not create account", e);
+        }
+        //    throw new UnsupportedOperationException("Unimplemented method 'createAccount'");
+    }
+        
     @Override
     public Optional<Account> getAccount(@NotBlank String accountId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAccount'");
+        try{
+            Account newAccount = accountDatabase.get(accountId);
+            return Optional.ofNullable(newAccount);
+        } catch (Exception e) {
+            log.error("Problem getting account",e);
+            throw new DatabaseException("Problem getting account", e);
+        }
+        //throw new UnsupportedOperationException("Unimplemented method 'getAccount'");
     }
 
     @Override
     public void updateAccount(@NotNull Account updatedAccount) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateAccount'");
+        //String accountID;
+        try {
+            //accountDatabase.put(updatedAccount.accountID, updatedAccount);
+            //log.info("Account updated successfully. Account ID: {}", updatedAccount.accountId);
+        } catch (Exception e) {
+            log.error("Error updating account", e);
+            throw new DatabaseException("Error updating account", e);
+        }
+      //  throw new UnsupportedOperationException("Unimplemented method 'updateAccount'");
     }
 
     @Override
     public void deleteAccount(@NotBlank String accountId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteAccount'");
+        try {
+            Account deletedAccount = accountDatabase.remove(accountId);
+
+            if (deletedAccount != null) {
+                log.info("Account deleted successfully. Account ID: {}", accountId);
+            } else {
+                log.warn("Account with ID {} not found for deletion.", accountId);
+            }
+        } catch (Exception e) {
+            log.error("Error deleting account", e);
+            throw new DatabaseException("Error deleting account", e);
+        }
+       // throw new UnsupportedOperationException("Unimplemented method 'deleteAccount'");
     }
 }
